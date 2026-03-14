e0bef712fd8ff446288850a0b80bff4de41aa71e
ViZi <selaawi7912@gmail.com>
2026-03-14 04:58:47 -0500

    Update outbound_call_example.sh

--- scripts/outbound_call_example.sh
+++ scripts/outbound_call_example.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+NUMBER=$1
+
+if [ -z "$NUMBER" ]; then
+  echo "Usage: ./outbound_call_example.sh <phone_number>"
+  exit 1
+fi
+
+echo "Originating outbound call to $NUMBER"
+
+asterisk -rx "channel originate PJSIP/1001 extension $NUMBER@outbound"
